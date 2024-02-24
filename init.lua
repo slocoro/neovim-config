9cc2bd793bf821cb0bd1df496081c735d60e2d70
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2024-02-24 12:14:21 +0000

    First commit.

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+
+vim.cmd('colorscheme onedark')
---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,8 @@
+require("core.plugin_config.gruvbox")
+require("core.plugin_config.catppuccin")
+require("core.plugin_config.onedark")
+require("core.plugin_config.lualine")
+require("core.plugin_config.nvim-tree") 
+require("core.plugin_config.nvim-web-devicons") 
+require("core.plugin_config.treesitter")
+require("core.plugin_config.telescope")
